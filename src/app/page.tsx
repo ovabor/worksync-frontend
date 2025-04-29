4f4c5fa903242267c525685f07badb2cb34c7160
lambeboluwatife <bouwatifelambe@gmail.com>
2025-04-29 23:01:27 +0100

    worksync benefit section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Benefit from "./pages/Home/Benefit";
 import Hero from "./pages/Home/Hero";
+import Team from "./pages/Home/Team";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Team />
+      <Benefit />
     </>
   );
 }
