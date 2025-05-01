943418db980ac66406fe4a0fe7b5fbdaacfead2a
lambeboluwatife <bouwatifelambe@gmail.com>
2025-05-01 19:22:57 +0100

    worksync simplify operations sections

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
 export const alliance = localFont({
-  src: './fonts/AllianceNo2-Regular.woff2',
+  src: './fonts/AllianceNo2-Bold.woff2',
   variable: '--font-alliance',
   display: 'swap',
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ProfessionalServices from "./pages/Home/ProfessionalServices";
 import TailoredSolution from "./pages/Home/TailoredSolution";
 import Team from "./pages/Home/Team";
 import FAQs from "./pages/Home/FAQs";
+import SimplifyOperations from "./pages/Home/SimplifyOperations";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Pricing />
       <ProfessionalServices />
       <FAQs />
+      <SimplifyOperations />
     </>
   );
 }
