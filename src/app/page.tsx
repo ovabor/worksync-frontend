9bd0c4935d5a95c46607853621f398f3f5243e6f
lambeboluwatife <bouwatifelambe@gmail.com>
2025-05-01 18:49:26 +0100

    worksync FAQs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Benefit from "./pages/Home/Benefit";
 import Hero from "./pages/Home/Hero";
 import Pricing from "./pages/Home/Pricing";
+import ProfessionalServices from "./pages/Home/ProfessionalServices";
 import TailoredSolution from "./pages/Home/TailoredSolution";
 import Team from "./pages/Home/Team";
+import FAQs from "./pages/Home/FAQs";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <Benefit />
       <TailoredSolution />
       <Pricing />
+      <ProfessionalServices />
+      <FAQs />
     </>
   );
 }
